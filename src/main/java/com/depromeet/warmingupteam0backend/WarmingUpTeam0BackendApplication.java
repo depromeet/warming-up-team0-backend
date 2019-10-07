00fb232343c21f854495c28944d7876138eda5a6
전해성 <seong0428@gmail.com>
2019-10-08 01:20:36 +0900

    Initial commit

---
+++ src/main/java/com/depromeet/warmingupteam0backend/WarmingUpTeam0BackendApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.warmingupteam0backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmingUpTeam0BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmingUpTeam0BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/warmingupteam0backend/WarmingUpTeam0BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.depromeet.warmingupteam0backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WarmingUpTeam0BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
